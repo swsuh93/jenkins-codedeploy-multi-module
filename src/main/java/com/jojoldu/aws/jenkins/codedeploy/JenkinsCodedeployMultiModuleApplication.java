ba261c4bf0d2935e5147d3b4bcebfaba303ee184
jojoldu <jojoldu@gmail.com>
2018-06-30 14:15:57 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/aws/jenkins/codedeploy/JenkinsCodedeployMultiModuleApplication.java
@@ -0,0 +1,12 @@
+package com.jojoldu.aws.jenkins.codedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCodedeployMultiModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCodedeployMultiModuleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jojoldu/aws/jenkins/codedeploy/JenkinsCodedeployMultiModuleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jojoldu.aws.jenkins.codedeploy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsCodedeployMultiModuleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
