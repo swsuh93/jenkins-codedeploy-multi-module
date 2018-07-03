639e7a042dc2569ff845af7d7e809682c023be4c
jojoldu <jojoldu@gmail.com>
2018-07-03 21:36:59 +0900

    codeDeploy yml, 스크립트 추가

---
+++ member-batch/code-deploy/jenkins.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+DEPLOY_DIR_NAME=code-deploy-${PROJECT_NAME}
+APP_NAME='dwlee-member'
+BUCKET='dwlee-member-deploy'
+ZIP_NAME=${PROJECT_NAME}-${GIT_COMMIT}-${BUILD_TAG}.zip
+
+./gradlew :${PROJECT_NAME}:clean :${PROJECT_NAME}:build
+
+echo "	> 배포.zip 생성"
+mkdir -p ${DEPLOY_DIR_NAME}
+cp ${PROJECT_NAME}/code-deploy/*.yml ${DEPLOY_DIR_NAME}/
+cp ${PROJECT_NAME}/code-deploy/*.sh ${DEPLOY_DIR_NAME}/
+cp ${PROJECT_NAME}/build/libs/*.jar ${DEPLOY_DIR_NAME}/
+
+cd ${DEPLOY_DIR_NAME}
+zip -r ${DEPLOY_DIR_NAME} *
+
+echo "	> AWS S3 업로드"
+aws s3 cp ${DEPLOY_DIR_NAME}.zip s3://${BUCKET}/${ZIP_NAME} --region ap-northeast-2
+
+echo "	> AWS CodeDeploy 배포"
+aws deploy create-deployment \
+--application-name ${APP_NAME} \
+--deployment-group-name ${PROJECT_NAME} \
+--region ap-northeast-2 \
+--s3-location bucket=${BUCKET},bundleType=zip,key=${ZIP_NAME}
+
+echo "	> 생성된 디렉토리 삭제"
+cd ..
+rm -rf ${DEPLOY_DIR_NAME}
